fcc4ecadaf82ed1a5f401d87b9ba1ca7d8524556
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2023-11-11 15:15:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ document.getElementById("checkButton").onclick = function(){
 var str2 = document.getElementById("link2").value;
 var str = str2;
 document.getElementById("result").innerHTML = "リンク　https://drive.google.com/uc?id="+str+"&export=download";
-  <script>
-        function result() {
+<script>
         var str = document.getElementById('text').value
         var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;正規表現（/〜/）を解釈してくれないエディタ等で自動整形を崩さないため。
         var regexp_makeLink = function(all, url, h, href) {
@@ -13,6 +12,6 @@ document.getElementById("result").innerHTML = "リンク　https://drive.google.
         var textWithLink = str.replace(regexp_url, regexp_makeLink);
         document.getElementById('result').innerHTML = textWithLink
         }
-    </script>
+</script>
 }
 }
