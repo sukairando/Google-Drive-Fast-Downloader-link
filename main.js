011e2bacdacb27c74dc6ccbf44ed4de32e7c105e
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2023-11-11 15:21:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.getElementById("result").innerHTML = "リンク　https://drive.google.
 <script>
         var str = document.getElementById('text').value
         var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;正規表現（/〜/）を解釈してくれないエディタ等で自動整形を崩さないため。
-        var regexp_makeLink = function(all, url, h, href) {
+        var regexp_makeLink = function(all, url, h, href)
         return '<a href="h' + href + '" target="_blank">' + url + '</a>';
         }
         var textWithLink = str.replace(regexp_url, regexp_makeLink);
