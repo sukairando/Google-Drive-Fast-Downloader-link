b297e26a1f14105dba4a8492b6a7d8a5066afeaf
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2023-11-11 15:02:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 window.onload = function(){
 document.getElementById("checkButton").onclick = function(){
-var str1 = document.getElementById("link1").value;
 var str2 = document.getElementById("link2").value;
-var str3 = document.getElementById("link3").value;
-var str = str1 + str2 +str3;
-document.getElementById("result").innerHTML = "リンク"+str+"";
+var str = str2;
+document.getElementById("result").innerHTML = "リンク　https://drive.google.com/uc?id="+str+"&export=download";
 }
 }
