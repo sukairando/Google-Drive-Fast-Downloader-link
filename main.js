56690ce58ca6af60d46ed11e723fbd9176133621
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2023-11-11 14:56:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 window.onload = function(){
 document.getElementById("checkButton").onclick = function(){
-var str1 = document.getElementById("uName1").value;
-var str2 = document.getElementById("uName2").value;
-var str = str1 + str2;
+var str1 = document.getElementById("link1").value;
+var str2 = document.getElementById("link2").value;
+var str3 = document.getElementById("link3").value;
+var str = str1 + str2 +str3;
 document.getElementById("result").innerHTML = "氏名は「"+str+"」です";
 }
 }
