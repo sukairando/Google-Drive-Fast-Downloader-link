61e1bd9ca55e8c5365c0c49f7aa3e2498eccf67e
스카이랜드 <83498837+sukairando@users.noreply.github.com>
2023-11-12 18:19:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 document.getElementById("checkButton").onclick = function(){
-var str2 = document.getElementById("link2").value;
-var str = str2;
+var str = document.getElementById("link2").value;
+var str = str;
 document.getElementById("result").innerHTML = "リンク　https://drive.google.com/uc?id="+str+"&export=download";
 }
 }
